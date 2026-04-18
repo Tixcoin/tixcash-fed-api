2d32da78d9bec1cef00d6361a372f60ec8a1da78
Alex Filippov <alex@binarycode.dev>
2026-04-18 09:00:00 +0300

    Remove dead InvokeAsync1 method that starts with unconditional return

--- SwaggerBasicAuthMiddleware.cs
+++ SwaggerBasicAuthMiddleware.cs
@@ -34,49 +34,6 @@ namespace dataAPI
                 await next.Invoke(context);
             }
         }
-        public async Task InvokeAsync1(HttpContext context)
-        {
-            return ;
-            if (context.Request.Path.StartsWithSegments("/WeatherForecast") || context.Request.Path.StartsWithSegments("/swagger"))
-            {
-                string authHeader = context.Request.Headers["Authorization"];
-                if (authHeader != null && authHeader.StartsWith("Basic "))
-                {
-                    var encodedUsernamePassword = authHeader.Split(' ', 2, StringSplitOptions.RemoveEmptyEntries)[1]?.Trim();
-                    var decodedUsernamePassword = Encoding.UTF8.GetString(Convert.FromBase64String(encodedUsernamePassword));
-
-                    var username = decodedUsernamePassword.Split(':', 2)[0];
-                    var password = decodedUsernamePassword.Split(':', 2)[1];
-
-                    if (!context.Request.Path.Value.Contains("Auth") || IsAuthorized(username, password))
-                    {
-                        await next.Invoke(context);
-                        return;
-                    }
-                }
-
-                //if (context.Request.Path.Value.Contains("Auth"))
-                //{
-                //    if (IsAuthorized("", ""))
-                //    {
-                //        context.Response.Headers["WWW-Authenticate"] = "Basic";
-                //        context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
-                //    }
-                //}
-                //else
-                //{
-                //   // await next.Invoke(context);
-                   
-                //}
-
-
-            }
-            else
-            {
-                await next.Invoke(context);
-            }
-        }
-
         public bool IsAuthorized(string username, string password)
         {
             return true;
